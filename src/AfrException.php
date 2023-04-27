f09c0c75d7183cf98dbfaafc6abb705ddb32baf6
Nistor Alexandru Marius <djthorr@gmail.com>
2023-04-28 00:18:07 +0300

    AfrException

--- src/AfrException.php
+++ src/AfrException.php
@@ -3,17 +3,46 @@ declare(strict_types=1);
 
 namespace Autoframe\Components\Exception;
 
+use Exception;
+use Throwable;
+
 /**
  * If you don't like the default exception class, you can use your own:
  * define('Autoframe\Components\Exception\SWAP_AFR_EXCEPTION','..dir../CustomAfrException.php');
  */
-
-if(defined('Autoframe\Components\Exception\SWAP_AFR_EXCEPTION')){
-    $phpInclude = constant('Autoframe\Components\Exception\SWAP_AFR_EXCEPTION');
+if(defined(__NAMESPACE__.'\SWAP_AFR_EXCEPTION')){
+    $phpInclude = constant(__NAMESPACE__.'\SWAP_AFR_EXCEPTION');
     if(file_exists($phpInclude)){
         include_once($phpInclude);
     }
 }
-if(!class_exists('Autoframe\Components\Exception\AfrException',false)){
-    include_once(__DIR__.DIRECTORY_SEPARATOR.'AfrException_.php');
+
+if(!class_exists(__NAMESPACE__.'\AfrException',false)){
+
+    class AfrException extends Exception
+    {
+        /**
+         * Construct the exception. Note: The message is NOT binary safe.
+         * Redefine the exception so message isn't optional
+         * @link https://php.net/manual/en/exception.construct.php
+         * @param string $message [optional] The Exception message to throw.
+         * @param int $code [optional] The Exception code.
+         * @param null|Throwable $previous [optional] The previous throwable used for the exception chaining.
+         */
+        public function __construct($message, $code = 0, Throwable $previous = null)
+        {
+            parent::__construct($message, $code, $previous);
+        }
+
+        /**
+         * String representation of the exception
+         * @link https://php.net/manual/en/exception.tostring.php
+         * @return string the string representation of the exception.
+         */
+        public function __toString()
+        {
+            return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+        }
+
+    }
 }
